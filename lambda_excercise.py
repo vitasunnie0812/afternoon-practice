86c410c3d35508a628e92351b535c38ffb45a3d0
sunny <lsunnyl89@gmail.com>
2022-06-28 01:13:29 -0600

    lambda exercise done

--- lambda_excercise.py
+++ lambda_excercise.py
@@ -28,15 +28,18 @@ list(filter(lambda x : 'a' in x[0] , prog_lang))
 #4. Filter the list so that it only contains languages whose version is in integer form.
 #[('Java', 13), ('JavaScript', 2019)]
 
-list(filter(lambda x : type(x[1])== int , prog_lang))
+list(filter(lambda x : type(x[1]) == int , prog_lang))
 
 
 #5. Transform the list so that it contains the tuples in the form,
 #("language in all lower case", length of the language string)
 #[('python', 6), ('java', 4), ('javascript', 10), ('scala', 5)]
 
-list(map(lambda x[0],x[1] : x[0]=x[0].lower() and x[1]=len(x[0])))
+list(map(lambda x : (x[0].lower(),len(x[0])), prog_lang))
 
 #6. Generate a tuple in the form,
 #("All languages separated by commas","All versions separated by commas")
 #('Python,Java,JavaScript,Scala', '3.8,13,2019,2.13')
+
+tuple =','.join(map(lambda x : x[0],prog_lang)),','.join(map(lambda x: str(x[1]), prog_lang))
+print(tuple)
